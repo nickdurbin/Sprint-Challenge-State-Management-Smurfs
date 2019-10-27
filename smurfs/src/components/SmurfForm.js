f5f4d603a0c2772e4b62dee2413a6b962a641a40
ndurbin <nick.durbin@gmail.com>
2019-10-27 11:48:40 -0400

    Created my form with 3 inputs for the name, height, and age of a smurf. Added component to APp.js.

---
+++ smurfs/src/components/SmurfForm.js
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import {createSmurf} from '../actions/smurfs';
+
+const SmurfForm = () => {
+  const dispatch = useDispatch()
+  const [newSmurf, setNewSmurf] = useState({
+    name: "",
+    age: "",
+    height: "",
+    id: ""
+  });
+
+  const handleChanges = e => {
+    setNewSmurf({
+      ...newSmurf,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    setNewSmurf({
+      name: "",
+      age: "",
+      height: "",
+      id: ""
+    });
+    dispatch(createSmurf(newSmurf));
+  };
+
+  return (
+    <div className='formContainer'>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="name"
+          placeholder="Name"
+          value={newSmurf.name}
+          onChange={handleChanges}
+        />
+        <input
+          type="text"
+          name="age"
+          placeholder="Age"
+          value={newSmurf.age}
+          onChange={handleChanges}
+        />
+        <input
+          type="text"
+          name="height"
+          placeholder="Height"
+          value={newSmurf.height}
+          onChange={handleChanges}
+        />
+        <button type="submit">
+          Add
+        </button>
+      </form>
+    </div>
+  )
+}
+
+export default SmurfForm;
